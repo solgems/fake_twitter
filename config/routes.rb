1ddd3aee190f1b10052c867060ecb472fa196533
Donald Lee <solgems@gmail.com>
2012-04-03 15:45:56 +0800

    fixed links to static pages, done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 FirstApp::Application.routes.draw do
-  get "static_pages/home"
+  # match '<desired path>', to: '<controller_name#action>'
 
-  get "static_pages/help"
+  # get "static_pages/home"
+  root to: 'static_pages#home'
   
-  get "static_pages/about"
+  # get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+  
+  # get "static_pages/about"
+  match '/about', to: 'static_pages#about'
+  
+  # get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact'
 
   resources :microposts
 
